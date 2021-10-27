3fc21b55775b3e26a6f08111adfa3c0ee957dadf
TimA314 <6e9c2c7t@mailer.me>
2021-10-27 11:16:55 +0900

    Added Comic Book  Repository

--- Controllers/ComicBooksController.cs
+++ Controllers/ComicBooksController.cs
@@ -11,23 +11,10 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            var comicBook = new ComicBook()
-            {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p> Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                    new Artist() { Name = "Dan Slott", Role = "Script" },
-                    new Artist() { Name = "Humberto Ramos", Role = "Pencils" },
-                    new Artist() { Name = "Victor Olazaba", Role = "Inks" },
-                    new Artist() { Name = "Edgar Delgado", Role = "Colors" },
-                    new Artist() { Name = "Chris Eliopoulos", Role = "Letters" }
-                }
-            };
+ 
             
 
-            return View(comicBook);
+            return View(null);
         }
     }
-}
+}   
